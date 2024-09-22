a7d12a1f02b7a5a81ad5a812155c2365b56ccdec
krish <krishmaniyar27@gmail.com>
2024-09-22 15:51:26 +0530

    index.html

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+
+// var player1 = prompt("Name of Player 1?");
+// console.log(player1);
+// var player2 = prompt("Name of Player 2?");
+// console.log(player2);
+
+// var box = [1,2,3,4,5,6,7,8,9]
+
+// function logic()
+// {
+//     var input1 = prompt("input1");
+//     var input2 = prompt("input2");
+// }
